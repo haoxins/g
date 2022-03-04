bb5bed8dcba1162e8163a4deb43a86124f482192
Hao Xin <haoxinst@gmail.com>
2022-03-04 15:06:35 +0800

    Export filter functions

--- filter.go
+++ filter.go
@@ -3,4 +3,14 @@ package g
 // I disagree with Rob Pike
 //   I just use "for" loops.
 //   You shouldn't use it either.
-import _ "robpike.io/filter"
+import "robpike.io/filter"
+
+var (
+	Apply         = filter.Apply
+	ApplyInPlace  = filter.ApplyInPlace
+	Choose        = filter.Choose
+	ChooseInPlace = filter.ChooseInPlace
+	Drop          = filter.Drop
+	DropInPlace   = filter.DropInPlace
+	Reduce        = filter.Reduce
+)
