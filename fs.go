e4de0f94d66c3b9c4b463f1a6ad22a3aa2a885ac
Hao Xin <haoxinst@gmail.com>
2022-05-11 23:42:46 +0800

    Add Exists

--- fs.go
+++ fs.go
@@ -5,14 +5,25 @@ import (
 	"os"
 )
 
-func CopyFile(from, dest string) error {
-	src, err := os.Open(from)
+func Exists(filepath string) (bool, error) {
+	_, err := os.Stat(filepath)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func CopyFile(source, target string) error {
+	src, err := os.Open(source)
 	if err != nil {
 		return err
 	}
 	defer src.Close()
 
-	dst, err := os.OpenFile(dest, os.O_WRONLY|os.O_CREATE, 0644)
+	dst, err := os.OpenFile(target, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return err
 	}
--- fs_test.go
+++ fs_test.go
@@ -10,4 +10,13 @@ var _ = Describe("Test fs", func() {
 		err := CopyFile("fs.go", "fs.go.out")
 		Expect(err).To(BeNil())
 	})
+
+	It("Exists should work", func() {
+		exists, err := Exists("fs.go")
+		Expect(err).To(BeNil())
+		Expect(exists).To(BeTrue())
+		exists, err = Exists("fs_.go")
+		Expect(err).To(BeNil())
+		Expect(exists).To(BeFalse())
+	})
 })
